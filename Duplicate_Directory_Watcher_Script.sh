93e0d5070636fff7ab73f21ad5365b022a45524e
Anthony <53560209+Anthony-T-N@users.noreply.github.com>
2023-09-02 20:36:54 +1000

    Update Duplicate_Directory_Watcher_Script.sh

--- Duplicate_Directory_Watcher_Script.sh
+++ Duplicate_Directory_Watcher_Script.sh
@@ -4,9 +4,9 @@
 
 log_path="./directory_watch.log"
 directory_watch_path_s=""
-second_directory_switch=false
+secondary_directory_switch=false
 
-while getopts ":p:s:d:h:" OPTION; do
+while getopts ":p:s:d:" OPTION; do
     case $OPTION in
         p)
             directory_watch_path="$OPTARG"
@@ -21,7 +21,7 @@ while getopts ":p:s:d:h:" OPTION; do
                 notify-send --expire-time=0 --urgency=critical -i ~/vcs-locally-modified-unstaged.svg "$(date +"%FT%I:%M:%S%p%Z")" "\- $directory_watch_path_s missing / \n- [!] END"
                 exit 1
             fi
-            second_directory_switch=true
+            secondary_directory_switch=true
             ;;
         d)
             re='^[0-9]+$'
@@ -32,7 +32,7 @@ while getopts ":p:s:d:h:" OPTION; do
             directory_watch_depth="$OPTARG"
             ;;
         *)
-            echo "Usage: $0 [-p directory_path] [-s directory_path] [-d directory_watch_depth]"
+            echo "Usage: $0 [-p directory_path] [-s secondary_directory_path] [-d directory_watch_depth]"
             exit 1
             ;;
     esac
@@ -41,27 +41,24 @@ done
 echo "directory_watch_path: $directory_watch_path"
 echo "directory_watch_path_s: $directory_watch_path_s"
 echo "directory_watch_depth: $directory_watch_depth"
-echo "second_directory_switch: $second_directory_switch"
+echo "secondary_directory_switch: $secondary_directory_switch"
 
 if [ ! -d "$directory_watch_path" ]; then
     notify-send --expire-time=0 --urgency=critical -i ~/vcs-locally-modified-unstaged.svg "$(date +"%FT%I:%M:%S%p%Z")" "\- $directory_watch_path does not exist.\n- [!] END"
     exit 1
 fi
 
-if [ $second_directory_switch == true ]; then
+if [ $secondary_directory_switch == true ]; then
     if [ ! -d "$directory_watch_path_s" ]; then
         notify-send --expire-time=0 --urgency=critical -i ~/vcs-locally-modified-unstaged.svg "$(date +"%FT%I:%M:%S%p%Z")" "\- $directory_watch_path_s does not exist.\n- [!] END"
         exit 1
     fi
 fi 
 
-#if [ "$#" > 2 ] ; then
-#fi
-
 # If ./directory_watch.log does not exist, record filenames of all files in watched directory at n directory depth.
 if [ ! -f $log_path ] ; then
     find $directory_watch_path -mindepth "$(($directory_watch_depth-1))" -maxdepth "$(($directory_watch_depth-1))" -type d | sort -V | while read -r line; do echo "$(date +"%FT%I:%M:%S%p%Z") CREATE,ISDIR $line" | tee -a $log_path; done
-    if [ $second_directory_switch == true ]; then
+    if [ $secondary_directory_switch == true ]; then
         find $directory_watch_path_s -mindepth "$(($directory_watch_depth-1))" -maxdepth "$(($directory_watch_depth-1))" -type d | sort -V | while read -r line; do echo "$(date +"%FT%I:%M:%S%p%Z") CREATE,ISDIR $line" | tee -a $log_path; done
     fi
     notify-send --expire-time=0 --urgency=critical -i ~/vcs-locally-modified-unstaged.svg "$(date +"%FT%I:%M:%S%p%Z")" "\- $log_path does not exist.\n- Captured current state of watched folder(s)\n- [!] END"
